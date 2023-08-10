d2b73d41f21f07f5b08c8a482d972e2771616cc1
John Sungjin Kim <jsjkim00@gmail.com>
2023-08-09 21:15:28 -0700

    modal webhook

--- server/README.md
+++ server/README.md
@@ -7,3 +7,10 @@ https://github.com/python-poetry/poetry/issues/6409
 What ended up working was the current `pyproject.toml`, but instead of running `poetry install`, running `poetry lock` first.
 
 Now that the lockfile is generated, though, it should be totally fine to just `poetry install`.
+
+Test curl:
+```
+curl -d '{"model_name": "hkunlp/instructor-large", "instruction": "Represent this sentence:", "text": "Hello world!"}' \
+    -H "Content-Type: application/json" \
+    -X POST https://village-dev--embedding-playground-generate-embedding-1d0363-dev.modal.run
+```
--- server/src/embeddings.py
+++ server/src/embeddings.py
@@ -1,14 +1,49 @@
 # %%
-from InstructorEmbedding import INSTRUCTOR
+from typing import Literal
 
-print("Loading Instructor model...")
-model = INSTRUCTOR("hkunlp/instructor-large")
-print("Model loaded!")
+import modal
+from InstructorEmbedding import INSTRUCTOR
+from pydantic import BaseModel
+
+Models = Literal["hkunlp/instructor-large"]
+
 
 # %%
-sentence = "3D ActionSLAM: wearable person tracking in multi-floor environments"
-instruction = "Represent the Science title:"
+def cache_models():
+    """
+    Saves all models to the Modal image.
+    """
+    _ = INSTRUCTOR("hkunlp/instructor-large")
+    return
 
-print("Encoding embeddings...")
-embeddings = model.encode([[instruction, sentence]])
-print(embeddings)
+
+image = (
+    modal.Image.debian_slim(python_version="3.11")
+    .poetry_install_from_file("pyproject.toml", without=["dev"])
+    .run_function(cache_models)
+)
+stub = modal.Stub("embedding-playground")
+
+
+# %%
+
+
+class GenerateEmbeddingsRequest(BaseModel):
+    model_name: Models
+    instruction: str | None
+    text: str
+
+
+class GenerateEmbeddingsResponse(BaseModel):
+    embeddings: list[float]
+
+
+@stub.function(image=image)
+@modal.web_endpoint(method="POST")
+def generate_embeddings(req: GenerateEmbeddingsRequest):
+    if req.model_name == "hkunlp/instructor-large":
+        model = INSTRUCTOR("hkunlp/instructor-large")
+        embeddings = model.encode([req.instruction or "", req.text])
+        return GenerateEmbeddingsResponse(embeddings=embeddings[1].tolist())
+    else:
+        raise ValueError("Model not supported.")
