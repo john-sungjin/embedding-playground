6b00b1baffcba69c880488475905ab9c81f19f74
John Sungjin Kim <jsjkim00@gmail.com>
2023-08-10 21:37:45 -0700

    fix gitignore

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -21,22 +21,29 @@ import {
   TrashIcon,
   PlusIcon,
 } from "@radix-ui/react-icons";
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { generateEmbeddings } from "./config";
-import { evaluate } from "mathjs";
+import math, { evaluate } from "mathjs";
 import { cosineSimilarity } from "./math";
 import { useToast } from "@/components/ui/use-toast";
 
-const VECTOR_PREFIX = "v";
+const TEXT_EMBED_PREFIX = "t";
+const MATH_EMBED_PREFIX = "m";
 
 const models = [
   { value: "hkunlp/instructor-large", label: "Instructor Large" },
 ];
 
-interface EmbeddingInfo {
+interface TextEmbeddingInfo {
   name: string;
   instruction: string;
   text: string;
+  embedding: number[] | null;
+}
+
+interface MathEmbeddingInfo {
+  name: string;
+  expression: string;
   embedding: number[] | null;
 }
 
@@ -44,18 +51,51 @@ export default function Home() {
   const { toast } = useToast();
   const [open, setOpen] = useState(false);
   const [modelValue, setModelValue] = useState<string | null>(null);
-  const [embeddingInfo, setEmbeddingInfo] = useState<EmbeddingInfo[]>([
+
+  const [textEmbeddingInfo, setTextEmbeddingInfo] = useState<
+    TextEmbeddingInfo[]
+  >([
     {
-      name: "v0",
+      name: TEXT_EMBED_PREFIX + "0",
       instruction: "",
       text: "",
       embedding: null,
     },
   ]);
-  const [mathExpression, setMathExpression] = useState<string>("");
+  const [mathEmbeddingInfo, setMathEmbeddingInfo] = useState<
+    MathEmbeddingInfo[]
+  >([
+    {
+      name: MATH_EMBED_PREFIX + "0",
+      expression: "",
+      embedding: null,
+    },
+  ]);
+
+  // Edit on change
+  const [textTimeoutId, setTextTimeoutId] = useState<number | null>(null);
+  const [mathTimeoutId, setMathTimeoutId] = useState<number | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (textTimeoutId) {
+        clearTimeout(textTimeoutId);
+      }
+      if (mathTimeoutId) {
+        clearTimeout(mathTimeoutId);
+      }
+    };
+  }, [textTimeoutId, mathTimeoutId]);
+
   const [isGenerating, setIsGenerating] = useState(false);
 
-  const generateEmbeddingsHandler = async () => {
+  const textEmbeddingsHandler = async (
+    textEmbeddingInfo: TextEmbeddingInfo,
+  ) => {
+    if (textTimeoutId) {
+      clearTimeout(textTimeoutId);
+    }
+
     // conditional logic to make sure fields are filled
     if (!modelValue) {
       toast({
@@ -69,17 +109,17 @@ export default function Home() {
     try {
       const response = await generateEmbeddings({
         embed_model_name: modelValue,
-        inputs: embeddingInfo.map((info) => ({
+        inputs: textEmbeddingInfo.map((info) => ({
           instruction: info.instruction,
           text: info.text,
         })),
       });
 
-      setEmbeddingInfo(
-        embeddingInfo.map((info, index) => ({
+      setTextEmbeddingInfo(
+        textEmbeddingInfo.map((info, index) => ({
           ...info,
           embedding: response.embeddings[index],
-        }))
+        })),
       );
     } catch (e) {
       toast({
@@ -91,24 +131,24 @@ export default function Home() {
     setIsGenerating(false);
   };
 
-  const evaluateHandler = () => {
+  const evaluateHandler = (expression: string) => {
     // 1. Add all embeddings to scope
-    const scope: Record<string, any> = embeddingInfo.reduce(
+    const scope: Record<string, any> = textEmbeddingInfo.reduce(
       (
         acc: {
           [key: string]: number[];
         },
         info,
-        index
+        index,
       ) => {
         if (!info.embedding) {
           // TODO: better error handling
           return acc;
         }
-        acc[`${VECTOR_PREFIX}${index}`] = info.embedding; // make sure this is not null
+        acc[`${TEXT_EMBED_PREFIX}${index}`] = info.embedding; // make sure this is not null
         return acc;
       },
-      {}
+      {},
     );
 
     // 2. Add cosine similarity function to scope
@@ -117,7 +157,7 @@ export default function Home() {
     console.log(scope);
 
     try {
-      const result = evaluate(mathExpression, scope);
+      const result = evaluate(expression, scope);
       console.log(result);
     } catch (e) {
       console.log(e);
@@ -125,149 +165,220 @@ export default function Home() {
   };
 
   return (
-    <main className="border p-4 flex-col space-y-4">
-      <h1 className="font-bold">Embedding Playground</h1>
-      {/* MODEL DROPDOWN START */}
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="w-72 justify-between"
-          >
-            {modelValue
-              ? models.find((model) => model.value === modelValue)?.label
-              : "Select embedding model..."}
-            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-72 p-0">
-          <Command>
-            <CommandInput placeholder="Search models..." />
-            <CommandEmpty>No model found.</CommandEmpty>
-            <CommandGroup>
-              {models.map((model) => (
-                <CommandItem
-                  key={model.value}
-                  value={model.value}
-                  onSelect={(currentValue) => {
-                    setModelValue(
-                      currentValue === modelValue ? "" : currentValue
-                    );
-                    setOpen(false);
+    <main className="flex h-screen">
+      {/* SIDEBAR START */}
+      <div className="flex h-full w-1/3 flex-col space-y-4 border p-4">
+        <h1 className="font-bold">Embedding Playground</h1>
+        {/* MODEL DROPDOWN START */}
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-72 justify-between"
+            >
+              {modelValue
+                ? models.find((model) => model.value === modelValue)?.label
+                : "Select embedding model..."}
+              <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-72 p-0">
+            <Command>
+              <CommandInput placeholder="Search models..." />
+              <CommandEmpty>No model found.</CommandEmpty>
+              <CommandGroup>
+                {models.map((model) => (
+                  <CommandItem
+                    key={model.value}
+                    value={model.value}
+                    onSelect={(currentValue) => {
+                      setModelValue(
+                        currentValue === modelValue ? "" : currentValue,
+                      );
+                      setOpen(false);
+                    }}
+                  >
+                    {model.label}
+                    <CheckIcon
+                      className={cn(
+                        "ml-auto h-4 w-4",
+                        modelValue === model.value
+                          ? "opacity-100"
+                          : "opacity-0",
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+        {/* MODEL DROPDOWN END */}
+        {/* TEXT EMBEDDINGS START */}
+        <h3>Text Embeddings</h3>
+        <div className="flex flex-col space-y-4">
+          {textEmbeddingInfo.map((info, index) => (
+            <div key={index} className="flex-col space-y-2">
+              {/* HEADER START */}
+              <div className="flex flex-row space-x-2">
+                <h5 className="flex h-8 w-fit items-center justify-center rounded-md bg-gray-100 px-2 font-mono text-sm text-gray-600">
+                  {info.name} ={" "}
+                  {info.embedding
+                    ? "[" +
+                      info.embedding
+                        .slice(0, 5)
+                        .map((e) => e.toFixed(5))
+                        .join(", ") +
+                      ", ...]"
+                    : "null"}
+                </h5>
+                <Button
+                  onClick={() => {
+                    const newEmbeddingInfo = [...textEmbeddingInfo];
+                    newEmbeddingInfo.splice(index, 1);
+                    setTextEmbeddingInfo(newEmbeddingInfo);
                   }}
+                  className="flex h-8 w-8 items-center justify-center border-red-300 hover:bg-red-100"
+                  variant="outline"
                 >
-                  {model.label}
-                  <CheckIcon
-                    className={cn(
-                      "ml-auto h-4 w-4",
-                      modelValue === model.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      {/* MODEL DROPDOWN END */}
-      {/* EMBEDDING INPUT START */}
-      <h3>Active Embeddings</h3>
-      <div className="flex flex-col space-y-4">
-        {embeddingInfo.map((info, index) => (
-          <div key={index} className="flex-col space-y-2">
-            {/* HEADER START */}
-            <div className="flex flex-row space-x-2">
-              <h5 className="flex items-center justify-center px-2 h-8 bg-gray-100 w-fit rounded-md font-mono text-gray-600 text-sm">
-                {info.name} ={" "}
-                {info.embedding
-                  ? "[" +
-                    info.embedding
-                      .slice(0, 5)
-                      .map((e) => e.toFixed(5))
-                      .join(", ") +
-                    ", ...]"
-                  : "null"}
-              </h5>
-              <Button
-                onClick={() => {
-                  const newEmbeddingInfo = [...embeddingInfo];
-                  newEmbeddingInfo.splice(index, 1);
-                  setEmbeddingInfo(newEmbeddingInfo);
-                }}
-                className="flex justify-center items-center w-8 h-8 hover:bg-red-100 border-red-300"
-                variant="outline"
-              >
-                <TrashIcon className="h-4 w-4 shrink-0 text-red-500" />
-              </Button>
-            </div>
-            {/* INPUT ROW START */}
-            <div className="flex flex-row space-x-4">
-              <Textarea
-                placeholder="Enter instruction..."
-                value={info.instruction || ""}
-                onChange={(e) => {
-                  const newEmbeddingInfo = [...embeddingInfo];
-                  newEmbeddingInfo[index].instruction = e.target.value;
-                  setEmbeddingInfo(newEmbeddingInfo);
-                }}
-              />
-              <Textarea
-                placeholder="Enter text..."
-                value={info.text || ""}
-                onChange={(e) => {
-                  const newEmbeddingInfo = [...embeddingInfo];
-                  newEmbeddingInfo[index].text = e.target.value;
-                  setEmbeddingInfo(newEmbeddingInfo);
-                }}
-              />
-            </div>
-            {/* EMBEDDING INPUT ROW END */}
-          </div>
-        ))}
-        <div className="flex items-center justify-center">
-          <Button
-            onClick={() => {
-              // check if name already exists
-              let newIndex = embeddingInfo.length;
-              while (
-                embeddingInfo.find(
-                  (info) => info.name === `${VECTOR_PREFIX}${newIndex}`
-                )
-              ) {
-                newIndex += 1;
-              }
-              setEmbeddingInfo([
-                ...embeddingInfo,
-                {
-                  name: `${VECTOR_PREFIX}${newIndex}`,
-                  instruction: "",
-                  text: "",
-                  embedding: null,
-                },
-              ]);
-            }}
-            variant="secondary"
-            className="p-2 h-8 w-8"
-          >
-            <PlusIcon className="h-4 w-4 text-gray-700" />
-          </Button>
-        </div>
-      </div>
+                  <TrashIcon className="h-4 w-4 shrink-0 text-red-500" />
+                </Button>
+              </div>
+              {/* HEADER END */}
+              {/* TEXT EMBEDDING INPUT START */}
+              <div className="flex flex-row space-x-4">
+                <Textarea
+                  placeholder="Enter instruction..."
+                  value={info.instruction}
+                  onChange={(e) => {}}
+                />
+                <Textarea
+                  placeholder="Enter text..."
+                  value={info.text}
+                  onChange={(e) => {
+                    const newEmbeddingInfo = [...textEmbeddingInfo];
+                    newEmbeddingInfo[index].text = e.target.value;
+                    setTextEmbeddingInfo(newEmbeddingInfo);
 
-      <Button onClick={generateEmbeddingsHandler}>
-        {isGenerating ? "Loading..." : "Generate Embeddings"}
-      </Button>
-      <h3>Vector Math</h3>
-      <Textarea
-        placeholder="Enter math..."
-        value={mathExpression}
-        onChange={(e) => {
-          setMathExpression(e.target.value);
-        }}
-      />
-      <Button onClick={evaluateHandler}>Calculate</Button>
+                    textEmbeddingsHandler();
+                  }}
+                />
+              </div>
+              {/* TEXT EMBEDDING INPUT END */}
+            </div>
+          ))}
+          <div className="flex items-center justify-center">
+            <Button
+              onClick={() => {
+                // check if name already exists
+                let newIndex = textEmbeddingInfo.length;
+                while (
+                  textEmbeddingInfo.find(
+                    (info) => info.name === `${TEXT_EMBED_PREFIX}${newIndex}`,
+                  )
+                ) {
+                  newIndex += 1;
+                }
+                setTextEmbeddingInfo([
+                  ...textEmbeddingInfo,
+                  {
+                    name: `${TEXT_EMBED_PREFIX}${newIndex}`,
+                    instruction: "",
+                    text: "",
+                    embedding: null,
+                  },
+                ]);
+              }}
+              variant="secondary"
+              className="h-8 w-8 p-2"
+            >
+              <PlusIcon className="h-4 w-4 text-gray-700" />
+            </Button>
+          </div>
+        </div>
+        {/* TEXT EMBEDDINGS END */}
+        {/* MATH EMBEDDINGS START */}
+        <h3>Math Embeddings</h3>
+        <div className="flex flex-col space-y-4">
+          {mathEmbeddingInfo.map((info, index) => (
+            <div key={index} className="flex-col space-y-2">
+              {/* HEADER START */}
+              <div className="flex flex-row space-x-2">
+                <h5 className="flex h-8 w-fit items-center justify-center rounded-md bg-gray-100 px-2 font-mono text-sm text-gray-600">
+                  {info.name} ={" "}
+                  {info.embedding
+                    ? "[" +
+                      info.embedding
+                        .slice(0, 5)
+                        .map((e) => e.toFixed(5))
+                        .join(", ") +
+                      ", ...]"
+                    : "null"}
+                </h5>
+                <Button
+                  onClick={() => {
+                    const newEmbeddingInfo = [...textEmbeddingInfo];
+                    newEmbeddingInfo.splice(index, 1);
+                    setTextEmbeddingInfo(newEmbeddingInfo);
+                  }}
+                  className="flex h-8 w-8 items-center justify-center border-red-300 hover:bg-red-100"
+                  variant="outline"
+                >
+                  <TrashIcon className="h-4 w-4 shrink-0 text-red-500" />
+                </Button>
+              </div>
+              {/* HEADER END */}
+              {/* TEXT EMBEDDING INPUT START */}
+              <div className="flex flex-row space-x-4">
+                <Textarea
+                  placeholder="Enter expression..."
+                  value={info.expression}
+                  onChange={(e) => {
+                    const newEmbeddingInfo = [...textEmbeddingInfo];
+                    newEmbeddingInfo[index].instruction = e.target.value;
+                    setTextEmbeddingInfo(newEmbeddingInfo);
+                  }}
+                />
+              </div>
+              {/* TEXT EMBEDDING INPUT END */}
+            </div>
+          ))}
+          <div className="flex items-center justify-center">
+            <Button
+              onClick={() => {
+                // check if name already exists
+                let newIndex = mathEmbeddingInfo.length;
+                while (
+                  mathEmbeddingInfo.find(
+                    (info) => info.name === `${MATH_EMBED_PREFIX}${newIndex}`,
+                  )
+                ) {
+                  newIndex += 1;
+                }
+                setMathEmbeddingInfo([
+                  ...mathEmbeddingInfo,
+                  {
+                    name: `${TEXT_EMBED_PREFIX}${newIndex}`,
+                    expression: "",
+                    embedding: null,
+                  },
+                ]);
+              }}
+              variant="secondary"
+              className="h-8 w-8 p-2"
+            >
+              <PlusIcon className="h-4 w-4 text-gray-700" />
+            </Button>
+          </div>
+        </div>
+        {/* MATH EMBEDDINGS END */}
+      </div>
+      {/* SIDEBAR END */}
+      {/* VIEW START */}
+      <div></div>
+      {/* VIEW END */}
     </main>
   );
 }
