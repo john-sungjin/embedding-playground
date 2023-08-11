b5dad8dc06a5f903b5a336834275675e37e41df9
John Sungjin Kim <jsjkim00@gmail.com>
2023-08-11 00:02:49 -0700

    fix ???

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*", // Proxy to Backend
+        destination: "http://127.0.0.1:8000/api/:path*", // Proxy to Backend
       },
     ];
   },
