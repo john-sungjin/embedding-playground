1c6df833e3384d67e4804f419c57c17b935521e8
Harshal Sheth <hsheth2@gmail.com>
2023-08-12 15:25:59 -0700

    fix bug in key

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -24,20 +24,18 @@ const Home = observer(() => {
       {/* SIDEBAR START */}
       <div className="flex h-full w-1/3 flex-col space-y-4 border-r p-4">
         <h1 className="font-bold">Embedding Playground</h1>
+        <ModelSelector modelValue={modelValue} setModelValue={setModelValue} />
         {/* TEXT EMBEDDINGS START */}
         <h3>Text Embeddings</h3>
-        <ModelSelector modelValue={modelValue} setModelValue={setModelValue} />
         <div className="flex flex-col space-y-4">
-          {Array.from(embedStore.textEmbeddings).map(
-            ([name, embedding], index) => (
-              <TextEmbeddingInput
-                key={index}
-                name={name}
-                model={modelValue}
-                embedding={embedding}
-              />
-            ),
-          )}
+          {Array.from(embedStore.textEmbeddings).map(([name, embedding]) => (
+            <TextEmbeddingInput
+              key={name}
+              name={name}
+              model={modelValue}
+              embedding={embedding}
+            />
+          ))}
           <div className="flex items-center justify-center">
             <Button
               onClick={embedStore.initTextEmbedding}
@@ -52,15 +50,9 @@ const Home = observer(() => {
         {/* MATH EMBEDDINGS START */}
         <h3>Math Embeddings</h3>
         <div className="flex flex-col space-y-4">
-          {Array.from(embedStore.mathEmbeddings).map(
-            ([name, embedding], index) => (
-              <MathEmbeddingInput
-                key={index}
-                name={name}
-                embedding={embedding}
-              />
-            ),
-          )}
+          {Array.from(embedStore.mathEmbeddings).map(([name, embedding]) => (
+            <MathEmbeddingInput key={name} name={name} embedding={embedding} />
+          ))}
           <div className="flex items-center justify-center">
             <Button
               onClick={embedStore.initMathEmbedding}
