8a44a63014259c49b374a3cac7141fdd52040a55
John Sungjin Kim <jsjkim00@gmail.com>
2023-08-11 19:59:29 -0700

    heatmap axis

--- client/components/SimilarityMatrix.tsx
+++ client/components/SimilarityMatrix.tsx
@@ -23,7 +23,6 @@ export const SimilarityMatrix: React.FC = observer(() => {
   useEffect(
     () =>
       autorun(() => {
-        console.log("RUNNING AUTORUN");
         const newSimilarities = new Map();
         embedStore.allValidEmbeddings.forEach((embedding, name) => {
           embedStore.allValidEmbeddings.forEach((otherEmbedding, otherName) => {
@@ -45,10 +44,10 @@ export const SimilarityMatrix: React.FC = observer(() => {
   const chartSettings = {
     width: 500,
     height: 500,
-    marginTop: 20,
-    marginRight: 20,
-    marginBottom: 20,
-    marginLeft: 20,
+    marginTop: 50,
+    marginRight: 50,
+    marginBottom: 50,
+    marginLeft: 50,
   };
 
   const { ref: chartRef, dims: chartDims } = useChartDimensions(chartSettings);
@@ -61,8 +60,8 @@ export const SimilarityMatrix: React.FC = observer(() => {
 
     const allNames = [...embedStore.allValidEmbeddings.keys()];
 
-    const xScale = d3.scaleBand(allNames, [0, 500]);
-    const yScale = d3.scaleBand(allNames, [0, 500]);
+    const xScale = d3.scaleBand(allNames, [0, chartDims.boundedWidth]);
+    const yScale = d3.scaleBand(allNames, [0, chartDims.boundedHeight]);
     const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([0, 1]);
 
     const rectangles = data.map(({ source, target, value }) => {
@@ -75,7 +74,7 @@ export const SimilarityMatrix: React.FC = observer(() => {
     });
 
     return { rectangles, xScale, yScale };
-  }, [similarities]);
+  }, [similarities, chartDims]);
 
   return (
     <div>
@@ -90,28 +89,50 @@ export const SimilarityMatrix: React.FC = observer(() => {
       })}
       {/* HEATMAP START */}
       <div ref={chartRef}>
-        <svg width={chartDims.width} height={chartDims.height}>
-          {rectangles.map(({ x, y, width, height, color }) => (
-            <rect
-              key={`${x}_${y}`}
-              x={x}
-              y={y}
-              width={width}
-              height={height}
-              fill={color}
-            />
-          ))}
-          <g y={500}>
-            {xScale.domain().map((name) => (
-              <text
-                key={name}
-                x={xScale(name)! + xScale.bandwidth() / 2}
-                y={-5}
-                textAnchor="middle"
-              >
-                {name}
-              </text>
+        <svg
+          width={chartDims.width}
+          height={chartDims.height}
+          className="border"
+        >
+          <g
+            transform={`translate(${chartDims.marginLeft}, ${chartDims.marginTop})`}
+          >
+            {rectangles.map(({ x, y, width, height, color }) => (
+              <rect
+                key={`${x}_${y}`}
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                fill={color}
+              />
             ))}
+            <g transform={`translate(0, ${chartDims.boundedHeight})`}>
+              {xScale.domain().map((name) => (
+                <text
+                  key={name}
+                  x={xScale(name)! + xScale.bandwidth() / 2}
+                  y={15}
+                  textAnchor="middle"
+                  fontSize={12}
+                >
+                  {name}
+                </text>
+              ))}
+            </g>
+            <g transform={`translate(-3, 0)`}>
+              {yScale.domain().map((name) => (
+                <text
+                  key={name}
+                  x={-3}
+                  y={yScale(name)! + yScale.bandwidth() / 2}
+                  textAnchor="end"
+                  fontSize={12}
+                >
+                  {name}
+                </text>
+              ))}
+            </g>
           </g>
         </svg>
       </div>
