ed518a279a67c1659597cb67249c81d9b6b7e1f4
Harshal Sheth <hsheth2@gmail.com>
2023-08-26 12:46:25 -0700

    add posthog w/ nextjs

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
         source: "/api/:path*",
         destination: "http://127.0.0.1:8000/api/:path*", // Proxy to Backend
       },
+      // Posthog proxy.
+      // Docs: https://posthog.com/docs/advanced/proxy/nextjs.
+      {
+        source: "/ingest/:path*",
+        destination: "https://app.posthog.com/:path*",
+      },
     ];
   },
 };
